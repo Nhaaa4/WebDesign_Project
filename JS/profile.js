47f4001815c33ef861dc4f657ae05a61e40867f7
SeilaCP <manhseila@gmail.com>
2024-12-11 15:55:05 +0700

    "header-footer.css responsive"

--- JS/profile.js
+++ JS/profile.js
@@ -1,5 +1,24 @@
 const profileHtml = document.getElementById('profile');
-let count = true;
+let count = true, mouse = true;
+
+let menuIcon =  document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+var x = window.matchMedia("(max-width: 800px)");
+var y = window.matchMedia("(min-width: 801px)");
+
+// function mouseover()
+// {
+//     if (x.matches) {
+//         if (mouse == true)
+//             {
+//                 navbar.style.display = 'grid';
+//                 mouse = false;
+//             }else{
+//                 navbar.style.display = 'none';
+//                 mouse = true;
+//             } 
+//     }
+// }
 
 function clickcloseAccount()
 {
